6df02ee11f239d8a14088e89f54fccfe415ac8b0
HanSeongLee <jkhanseong@gmail.com>
2022-09-17 23:31:20 +0900

    ADD | sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, err }: AppProps) {
+  return <Component {...pageProps}
+                    error={err}
+  />;
 }
 
 export default MyApp
