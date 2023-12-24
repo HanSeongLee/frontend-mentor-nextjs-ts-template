3787a4c689455c6c6c3be2fc11c22cb78447e93c
HanSeongLee <jkhanseong@gmail.com>
2023-12-24 19:28:48 +0900

    feat: add next-seo

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import SEO from 'config/next-seo.config';
+import { DefaultSeo } from 'next-seo';
+import Head from 'next/head';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <meta name="viewport"
+                      content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <DefaultSeo {...SEO} />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp
--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -1,19 +1,16 @@
 import {Head, Html, Main, NextScript} from 'next/document';
-import DefaultHead from 'components/commons/DefaultHead';
 
 function Document() {
     return (
         <Html>
-            <Head>
-                <DefaultHead />
-            </Head>
+            <Head />
             <body>
                 <Main />
                 <NextScript />
             </body>
         </Html>
     );
-};
+}
 
 
 export default Document;
