1569c5c0d0bc23265ef2def798290db89db091b1
HanSeongLee <jkhanseong@gmail.com>
2024-11-17 17:49:21 +0900

    fix(commitlint.config): remove some rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'header-max-length': [2, 'never', 72],
-        'body-leading-blank': [2, 'never'],
-        'body-max-line-length': [1, 'never', 100],
-        'body-max-length': [1, 'never', 400],
-        'footer-leading-blank': [2, 'never'],
         'type-enum': [
             2,
             'always',
