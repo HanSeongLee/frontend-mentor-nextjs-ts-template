eaeff395c05920e94e95290f0f40bba1ce820791
HanSeongLee <jkhanseong@gmail.com>
2024-11-27 23:00:08 +0900

    fix(components/ClientStack): fix ssr match error

--- src/components/ClientStack/index.tsx
+++ src/components/ClientStack/index.tsx
@@ -2,10 +2,16 @@
 
 import React from 'react';
 import { Stack } from '@/stackflow';
+import { usePathname } from 'next/navigation';
 
 const ClientStack: React.FC = () => {
+    const pathname = usePathname();
     return (
-        <Stack />
+        <Stack initialContext={{
+            req: {
+                path: pathname,
+            }
+        }} />
     );
 };
 
