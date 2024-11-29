c78cc96c01e99bad83fa33f89c8af7f68a5b213a
HanSeongLee <jkhanseong@gmail.com>
2024-11-29 17:17:08 +0900

    feat(components/ClientStack): add search parameters

--- src/components/ClientStack/index.tsx
+++ src/components/ClientStack/index.tsx
@@ -3,19 +3,21 @@
 import React from 'react';
 import { Stack } from '@/stackflow';
 import { usePathname } from 'next/navigation';
+import { useSearchParams } from 'next/dist/client/components/navigation';
 
 interface IClientStackProps {
-    params: any;
+
 }
 
-const ClientStack: React.FC<IClientStackProps> = ({ params }) => {
+const ClientStack: React.FC<IClientStackProps> = () => {
     const pathname = usePathname();
+    const searchParams = useSearchParams();
+
     return (
         <Stack initialContext={{
             req: {
-                path: pathname,
+                path: `${pathname}${searchParams.size > 0 ? `?${searchParams.toString()}` : ''}`,
             },
-            pageProps: params,
         }}
         />
     );
