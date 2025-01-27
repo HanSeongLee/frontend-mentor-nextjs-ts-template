bf8ffdf0afe708393a61d19a86551172eaed0ee6
HanSeongLee <jkhanseong@gmail.com>
2025-01-27 17:31:55 +0900

    chore(next.config.js): remove sentry property in nextConfig

--- next.config.js
+++ next.config.js
@@ -22,15 +22,6 @@ const nextConfig = {
         return config;
     },
   // pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js', 'api.ts', 'api.js'],
-  sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    hideSourceMaps: true,
-  },
 }
 
 module.exports = withSentryConfig(
